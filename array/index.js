177365032374d76c47f0acb79b9a15b3fdf32981
HebaAlhamaydh <eng_heba_hamaydh@hotmail.com>
2023-03-30 06:56:38 +0000

    do

--- array/index.js
+++ array/index.js
@@ -31,22 +31,22 @@ console.log(numReplacements); // Output: 2
 
 // given an integer array count the number of element that occur more than once 
 function countDuplicateElements(arr) {
-    const counts = {};
+    const obj = {};
     let duplicates = 0;
   
     // count the occurrence of each element in the array
     for (let i = 0; i < arr.length; i++) {
-      if (counts[arr[i]] ) {
-        counts[arr[i]]++;
+      if (obj[arr[i]] ) {
+        obj[arr[i]]++;
        
       } else {
-        counts[arr[i]] = 1;
+        obj[arr[i]] = 1;
       }
     }
   
     // count the number of elements that occur more than once
-    for (let key in counts) {
-      if (counts[key] > 1) {
+    for (let key in obj) {
+      if (obj[key] > 1) {
         duplicates++;
       }
     }
